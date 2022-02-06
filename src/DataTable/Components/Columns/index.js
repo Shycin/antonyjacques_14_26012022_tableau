1ba51cb4445d7400590b4ddce29268e3a4b7cf0d
antony <antony.jacques06@gmail.com>
2022-02-06 16:30:57 +0100

    sort reset

--- src/DataTable/Components/Columns/index.js
+++ src/DataTable/Components/Columns/index.js
@@ -15,7 +15,11 @@ const Columns = function(props){
                                 <div className='column__title'>{element.title}</div>
                                 <div className='column__order' 
                                     onClick={(e) => 
-                                        sort.key === element.data ? setSort({...sort, direction: sort.direction*-1 }) : setSort({...sort,  key: element.data, direction: 1 })
+                                        sort.key === element.data 
+                                        ? sort.direction === 1 
+                                            ? setSort({...sort, direction: -1 }) 
+                                            : setSort({...sort, key: null })
+                                        : setSort({...sort,  key: element.data, direction: 1 })
                                     }>
                                     { (sort.key === element.data && sort.direction === 1) || (sort.key !== element.data) ? <div className='column__order__item order_up'>{order_up}</div> : ''}
                                     { (sort.key === element.data && sort.direction === -1) || (sort.key !== element.data) ? <div className='column__order__item order_down'>{order_down}</div> : ''}
